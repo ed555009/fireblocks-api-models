6d0d2bebd69dab4479b3703195a4c51c20ead72f
Ed Wang <ed555009@gmail.com>
2022-10-20 12:00:11 +0800

    add property summaries

--- src/Fireblocks.Api.Models/Requests/Params/GetPublicKeyInfoParams.cs
+++ src/Fireblocks.Api.Models/Requests/Params/GetPublicKeyInfoParams.cs
@@ -4,6 +4,13 @@ namespace Fireblocks.Api.Models.Requests.Params;
 
 public class GetPublicKeyInfoParams : GetPublicKeyInfoForVaultAccountParams
 {
+	/// <summary>
+	/// String, one of the supported SigningAlgorithms
+	/// </summary>
 	public SigningAlgorithm? algorithm { get; set; }
+
+	/// <summary>
+	/// List of integers
+	/// </summary>
 	public IEnumerable<int>? derivationPath { get; set; }
 }
